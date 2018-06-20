09e7ce08a4c17bd834db0cf8338bcfc6bddc85ba
hezhl <39982518+hezhl@users.noreply.github.com>
2018-06-20 15:39:24 +0800

    2

--- src/main/java/example/One.java
+++ src/main/java/example/One.java
@@ -2,9 +2,9 @@ package example;
 
 public class One {
     String message = "foo";
-    String message2 = "foo2";
+    
     public String foo() {
-        return message+message2;
+        return message;
     }
 
 }
